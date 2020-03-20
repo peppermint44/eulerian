113a2a6e7d2b0f036122fe3507fbf42bb887a544
Alex Wason <Alex@sci-fi.attlocal.net>
2020-03-20 17:51:03 -0400

    sum of even fibonacci numbers below 4,000,000

--- problem-2.py
+++ problem-2.py
@@ -1,14 +1,20 @@
+def divisible_by_2(a):
+    if a%2 == 0:
+        return True
+    else:
+        return False
+
 def fibonacci():
-    a=1
-    b=2
-    count=2
+    a=0
+    b=1
+    sum=0
     while a+b<4000000:
-        count=count+1
+        f=a+b
+        if divisible_by_2(f):
+            sum=sum+f
         c=a
         a=b
         b=c+b
-    print(a+b)
-
-
+    print(sum)
 
 fibonacci()
