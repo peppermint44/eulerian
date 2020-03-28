56a24edb0656536abf1df157fc6087ce6613f1a1
Alex Wason <Alex@Sci-Fi.lan1>
2020-03-28 17:21:46 -0400

    finished problem 3

--- problem-3.py
+++ problem-3.py
@@ -10,9 +10,31 @@ def divisible_by(a,b):
     else:
         return False
 
-def prime_factors(a):
-    for b in range(2,a/2):
-        if divisible_by(a,b) and is_prime(b):
-            print(b)
+# def divisible_by_any(a,list):
+#     for b in list:
+#         if divisible_by(a,b):
+#             return True
+#     return False
 
-is_prime(600851475143)
+def get_prime_factors(a):
+    prime_factors = {}
+    b = 2
+    while a > 1:
+        print("checking: ", b)
+        if is_prime(b) and divisible_by(a,b):
+            # we found a prime factor of a!!
+            prime_factors[b] = 1
+
+            a = a/b
+            while divisible_by(a,b):
+                prime_factors[b] = prime_factors[b] + 1
+                a = a/b
+
+            print(b, "divides the original 'a'", prime_factors[b], "times")
+
+        b = b + 1
+
+    prime_factors = {int(k) : v for k, v in prime_factors.items()}
+    return prime_factors
+
+print(get_prime_factors(999992))
