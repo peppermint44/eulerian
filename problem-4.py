106da768102df8589b1a9adf63771f382ab84d3b
Alex Wason <Alex@sci-fi.attlocal.net>
2020-03-29 19:01:03 -0400

    largest palindrome of range

--- problem-4.py
+++ problem-4.py
@@ -1,3 +1,5 @@
+    # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91*99.
+    # Find the largest palindrome made from the product of two 3-digit numbers.
 def reverse(a):
     b=0
     while a>0:
@@ -6,7 +8,17 @@ def reverse(a):
         a=a/10
     return b
 
-def is_palendrome(a):
+def is_palindrome(a):
     return reverse(a) == a
 
-print(is_palendrome(38183))
+def largest_palindrome_as_product(low,high):
+    largest_palindrome=0
+    for x in range (low,high):
+        for y in range (low,high):
+            z=x*y
+            if is_palindrome(z):
+                if z>largest_palindrome:
+                    largest_palindrome=z
+    return largest_palindrome
+   
+print(largest_palindrome_as_product(100,1000))
